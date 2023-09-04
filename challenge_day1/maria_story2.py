8bdd71cef3e3e0eb43f78fb50056de7422cc12db
mariapenad <mariajosepenad@gmail.com>
2023-09-04 16:44:16 +0200

    paragraph 3 act 2 (paragraph 9) paragraph 3 act 3 (paragraph 15)

---
+++ challenge_day1/maria_story2.py
@@ -0,0 +1,4 @@
+def text_9():
+    return("At the restaurant Amelia and Adam ordered the same ice cream that Steve ate because they were jealous that he could fly. They wanted to be birds also. They were uondering if they should bringe some for Max, Charlotte, and Eva. ")
+          
+text_9()
