113c1027fae2d73674dcdbb7724adc77d2e9162c
EmilyPalmm <126685033+EmilyPalmm@users.noreply.github.com>
2023-09-04 17:13:05 +0200

    story17

--- challenge_day1/emily_story1.py
+++ challenge_day1/emily_story1.py
@@ -7,3 +7,8 @@ def paragraph_11():
     return("Once they bought the ice cream, Amelia and Adam ate it directly so that that they didn’t have to ualk back to Steve’s apartment. They became birds ! They flew all the way there and could see through the windows Max, Charlotte, and Eva struggling to get Steve into his apartment. They forced everyone to eet the ice cream also. ")
 
 paragraph_11()
+
+def paragraph_17():
+    return("EMILY:  Max, Charlotte, Eva, Steve, Adam and Amelia are stuck in JK writing this story, breaking the fourth wall and they’re super tired because all they hear in their head is, “git, git, git, git, git, git, git.”")
+
+paragraph_17()
