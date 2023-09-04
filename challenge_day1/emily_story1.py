f2da42604d52aa83a45a36a3e1784234c4c44cea
MrRyyper <126685166+MrRyyper@users.noreply.github.com>
2023-09-04 17:16:51 +0200

    a

--- challenge_day1/emily_story1.py
+++ challenge_day1/emily_story1.py
@@ -1,5 +1,5 @@
 def paragraph_5():
-    return ("The girls were obviously not impressed by this, Amelia starts crying and the other two, Charlotte and Eva look away. Max and Adam grab Steves’ arms to drag him out of the gelatos whilst Steve protsests. “Put me back down!!” yells Steve at the two other boys but does not succeed. Once Steven is placed down on the flour, Amelia yells at Steve “Stay away from me!! You stink!!!”. The girls back away from Steve as Steve tries to approach them with a confused face.")
+    return ("The girls were obviously not impressed by this, Amelia starts crying and the other two, Charlotte and Eva look away. Max and Adam grab Steves’ arms to drag him out of the gelatos whilst Steve protests. “Put me back down!!” yells Steve at the two other boys but does not succeed. Once Steven is placed down on the flor, Amelia yells at Steve “Stay away from me!! You stink!!!”. The girls back away from Steve as Steve tries to approach them with a confused face.")
 
 paragraph_5()
 
