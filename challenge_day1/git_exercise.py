48de22fc9999c6dcfdbbc2b244f070805cd8ff11
Celia <celia.meissner@student.uva.nl>
2023-09-04 14:37:11 +0200

    file edited

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,8 +1,9 @@
 import sabrina
 import maria_string
+import celia_name
 
 def team_introduction():
-    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria_string.marias_name())
+    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria_string.marias_name(), celia_name.name())
 
 print(team_introduction())
 
