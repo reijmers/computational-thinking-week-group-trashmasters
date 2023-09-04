ba2a00ba5418a71d78be3f98178da8bcfeb43a4f
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 14:20:30 +0200

    trying to fix

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,19 +1,8 @@
 import sabrina
-<<<<<<< HEAD
 import maria
 
 def team_introduction():
-<<<<<<< HEAD
-    return print("This is Team Trashmasters. We are:", sabrina.sabrina_name())
-=======
     return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria.marias_name())
->>>>>>> a44bc3cea342af82810cc27d2036d2c16b660d3c
-=======
-import maria_string
-
-def team_introduction():
-    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria_string.marias_name())
->>>>>>> e72c28197c8ed6d1376b9f871d6fe699e8d76523
 
 print(team_introduction())
 
