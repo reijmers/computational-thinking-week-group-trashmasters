c481ef68899d87bad00ffd9ea41a5e15f51489d8
Celia <celia.meissner@student.uva.nl>
2023-09-04 15:41:54 +0200

    first paragraph done

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -4,12 +4,15 @@ import celia_name
 import emily
 import gur_string
 
+#imports for the story:
+import celia_story1 # paragraph 1 act 1
+
 def team_introduction():
-    return "This is Team Trashmasters. We are " + sabrina.sabrina_name() + ", " + maria_string.marias_name() + ", " + celia_name.name() + ", " + gur_string.gur_name() + ", and " + emily.emily() + "."
+    return "This is Team Trashmasters. We are " + sabrina.sabrina_name() + ", " + maria_string.marias_name() + ", " + celia_name.name() + ", " + gur_string.gur_name() + ", and " + emily.emily() + "." 
 
 print(team_introduction())
 
 #story:
 print("Act 1: The beginning of the night")
 #paragraph 1: celia
-print("Once upon a time Max, Adam, and Steve all went out to have dinner at an Italian Restaurant. When they were there they ordered a lot of food and drinks. Next too them was a table where Amelia, Eva, and Charlotte were celabrating Eva’s birthday. They also ordered a lot of food and drinks. The night was going to be long for Max, Adam, Steve, Amelia, Eva, and Charlotte. ")
+print(celia_story1.text())
