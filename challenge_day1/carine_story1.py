4962042ca34a0ec758a12338bfe1b87809e263f3
Celia <celia.meissner@student.uva.nl>
2023-09-04 18:16:54 +0200

    gr

--- challenge_day1/carine_story1.py
+++ challenge_day1/carine_story1.py
@@ -1,5 +1,5 @@
 #act1
 def text_6():
-    return("Within seconds, Steve collapse onto the ground. “Steve!” yells out Amelia and Charlotte, as Max and adam immediately rush to his side. A minute passes before Steve regains consciousness. As Charlotte is about to ask if Steve is okay, he jumps right back up and starts chirping like a bird! The rest stand in horror as they wonder what could possibly be going on. Steve doesn’t stop chirping and also begins to flap his arms. Adam says in disbelief, “I think Steve thinks he’s a bird!” \n")
+    return("Within seconds, Carine collapse onto the ground. Carine!” yells out Amelia and Charlotte, as Max and adam immediately rush to his side. A minute passes before Carine regains consciousness. As Charlotte is about to ask if Steve is okay, he jumps right back up and starts chirping like a bird! The rest stand in horror as they wonder what could possibly be going on. Steve doesn’t stop chirping and also begins to flap his arms. Adam says in disbelief, “I think Carine thinks he’s a bird!” \n")
 text_6()
 
--- challenge_day1/carine_story23.py
+++ challenge_day1/carine_story23.py
@@ -1,9 +1,9 @@
 #act2
 def text_12():
-    return("Now, everyone had eaten the same ice cream and gotten the same effects as Steve. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become humans again. \n")
+    return("Now, everyone had eaten the same ice cream and gotten the same effects as Carine. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become humans again. \n")
 text_12()
 
 #act 3
 def text_18():
-    return("This is the story of Max, Charlotte, Eva, Steve, Adam and Amelia. The End. \n")
+    return("This is the story of Max, Charlotte, Eva, Carine, Adam and Amelia. The End. \n")
 text_18()
