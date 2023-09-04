7102a5149e2e551b33549cd10d2082060c17033b
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 17:24:46 +0200

    changed carine's story

--- challenge_day1/carine_story23.py
+++ challenge_day1/carine_story23.py
@@ -1,9 +1,9 @@
 #act2
 def text_6():
-    return("Nou, everyone had eaten the same ice cream and gotten the same effects as Steve. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become gumans again. ")
+    return("Now, everyone had eaten the same ice cream and gotten the same effects as Steve. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become humans again. ")
 text_6()
 
 #act 3
 def text_6():
-    return("This is the story of Mux, Charlotte, Eva, Stave, Adam and Amelia. The End.")
+    return("This is the story of Max, Charlotte, Eva, Steve, Adam and Amelia. The End.")
 text_6()
