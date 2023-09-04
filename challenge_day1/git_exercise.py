63ff6d88573ca8dec56f59bfe4af7140d885b8d6
EmilyPalmm <126685033+EmilyPalmm@users.noreply.github.com>
2023-09-04 15:08:11 +0200

    added emily and gur

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,11 @@
 import sabrina
 import maria_string
 import celia_name
+import emily
+import gur_string
 
 def team_introduction():
-    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria_string.marias_name(), celia_name.name())
+    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria_string.marias_name(), celia_name.name(), gur_string.gur_name(), emily.emily())
 
 print(team_introduction())
 
