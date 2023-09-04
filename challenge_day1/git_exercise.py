c2d8ccf91b8d6bce24eff39991be51a2a3f0bef0
Celia <celia.meissner@student.uva.nl>
2023-09-04 15:28:12 +0200

    story

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -9,3 +9,7 @@ def team_introduction():
 
 print(team_introduction())
 
+#story:
+print("Act 1: The beginning of the night")
+#paragraph 1: celia
+print("Once upon a time Max, Adam, and Steve all whent out to have dinner at an Italian Restaurant. When thay were there they ordered a lot of food and drinks. Next too them was a table where Amelia, Eva, and Charlotte were celabrating Eva’s birthday. They also ordered a lot of food and drinks. The night was going to be long for Max, Adam, Steve, Amelia, Eva, and Charlotte. ")
