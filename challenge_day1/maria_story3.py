4c94720878a686c145eac6e9341d4cf71d2dfea3
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 17:17:37 +0200

    fixed maria story 3

--- challenge_day1/maria_story3.py
+++ challenge_day1/maria_story3.py
@@ -1,4 +1,4 @@
 def text_15():
-    return("Peter realises he’s taking to objects that no longer comprehend what hi’s saying because no one understands each other in the afterlife. But also everyone understands each other in the afterlife. It’s the good place. And the bad. What a plase.  Max, Charlotte, Eva, Steve, Adam and Amelia have seased to be who they once were.")
+    return("Peter realises he’s talking to objects that no longer comprehend what his saying because no one understands each other in the afterlife. But also everyone understands each other in the afterlife. It’s the good place. And the bad. What a place.  Max, Charlotte, Eva, Steve, Adam and Amelia have ceased to be who they once were.")
           
 text_15()
