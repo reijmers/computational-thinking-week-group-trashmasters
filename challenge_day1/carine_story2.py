7e110636293d8732f7e04aeb395d87506cd625a6
Carine Reijmers <carinereijmers@gmail.com>
2023-09-04 17:01:37 +0200

    file 2

---
+++ challenge_day1/carine_story2.py
@@ -0,0 +1,3 @@
+def text_6():
+    return("Nou, everyone had eaten the same ice cream and gotten the same effects as Steve. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become gumans again. ")
+text_6()
