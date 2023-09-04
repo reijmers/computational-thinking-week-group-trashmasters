b547e3b9ea839cd3d829901ebf11db692d3ae2fd
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 15:57:39 +0200

    fixed the string thing

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -3,16 +3,6 @@ import maria_string
 import celia_name
 import emily
 import gur_string
+import carine
 
-#imports for the story:
-import celia_story1 # paragraph 1 act 1
-
-def team_introduction():
-    return "This is Team Trashmasters. We are " + sabrina.sabrina_name() + ", " + maria_string.marias_name() + ", " + celia_name.name() + ", " + gur_string.gur_name() + ", and " + emily.emily() + "." 
-
-print(team_introduction())
-
-#story:
-print("Act 1: The beginning of the night")
-#paragraph 1: celia
-print(celia_story1.text())
+print(sabrina.team_introduction(carine.carine_name(), celia_name.name(), emily.emily(), gur_string.gur_name(), maria_string.marias_name(), sabrina.sabrina_name()))
