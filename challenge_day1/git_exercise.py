11677c746491c2082984a3d761fdd6316079402c
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 14:17:41 +0200

    trying to fix my git

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,7 +1,12 @@
 import sabrina
+import maria
 
 def team_introduction():
+<<<<<<< HEAD
     return print("This is Team Trashmasters. We are:", sabrina.sabrina_name())
+=======
+    return print("This is Team Trashmasters. We are", sabrina.sabrina_name(), maria.marias_name())
+>>>>>>> a44bc3cea342af82810cc27d2036d2c16b660d3c
 
 print(team_introduction())
 
