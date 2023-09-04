e197e0845c4ad3a4ea3e3f8c6a24ad2c2ccd5cbc
Carine Reijmers <carinereijmers@gmail.com>
2023-09-04 17:17:12 +0200

    act23

--- challenge_day1/carine_story1.py
+++ challenge_day1/carine_story1.py
@@ -2,13 +2,3 @@
 def text_6():
     return("Within seconds, Steve collapse onto the ground. “Steve!” yells out Amelia and Charlotte, as Max and adam immediately rush to his side. A minute passes before Steve regains consciousness. As Charlotte is about to ask if Steve is okay, he jumps right back up and starts chirping like a bird! The rest stand in horror as they wonder what could possibly be going on. Steve doesn’t stop chirping and also begins to flap his arms. Adam says in disbelief, “I think Steve thinks he’s a bird!” ")
 text_6()
-
-#act2
-def text_6():
-    return("Nou, everyone had eaten the same ice cream and gotten the same effects as Steve. Currently, Max, Charlotte, Eva, Adam, and Amelia were also birds flying all around the building with Max. However, Max became worried because he did not know when they’d become gumans again. ")
-text_6()
-
-#act 3
-def text_6():
-    return("This is the story of Mux, Charlotte, Eva, Stave, Adam and Amelia. The End.")
-text_6()
