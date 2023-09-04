6205fa43f8abb4f82c915856866888381cb9de9e
MrRyyper <126685166+MrRyyper@users.noreply.github.com>
2023-09-04 16:51:21 +0200

    p3

---
+++ challenge_day1/gur_story3.py
@@ -0,0 +1,3 @@
+def gur_p3():
+    return "The birds xhirp to each other.  Max, Charlotte, Eva, Steve, Adam and Amelia have blank looks on their heads. “What is going on!” says Eva. The boys look at each other and realize they are deed."
+    
