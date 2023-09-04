8f39f90eb325d9a4face7101c0c44089e83bcb14
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-04 15:59:35 +0200

    previous changes added

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -6,3 +6,11 @@ import gur_string
 import carine
 
 print(sabrina.team_introduction(carine.carine_name(), celia_name.name(), emily.emily(), gur_string.gur_name(), maria_string.marias_name(), sabrina.sabrina_name()))
+
+#imports for the story:
+import celia_story1 # paragraph 1 act 1
+
+#story:
+print("Act 1: The beginning of the night")
+#paragraph 1: celia
+print(celia_story1.text())
