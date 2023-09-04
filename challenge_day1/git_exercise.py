ee2bfddacb53b7be57c4364174e43a0ff28a561b
mariapenad <mariajosepenad@gmail.com>
2023-09-04 16:21:42 +0200

    paragraph 3 act 1

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -10,7 +10,12 @@ print(sabrina.team_introduction(carine.carine_name(), celia_name.name(), emily.e
 #imports for the story:
 import celia_story1 # paragraph 1 act 1
 
+import maria_story1 # paragraph 3 act 1
+
 #story:
 print("Act 1: The beginning of the night")
 #paragraph 1: celia
 print(celia_story1.text())
+
+#paragraph 3: maria
+print(maria_story1.text_3())
