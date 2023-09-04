ea294bacd8b4329a37695ca41a964e04558dba58
MrRyyper <126685166+MrRyyper@users.noreply.github.com>
2023-09-04 18:12:16 +0200

    aa

--- challenge_day1/gur_story3.py
+++ challenge_day1/gur_story3.py
@@ -1,5 +1,5 @@
 def gur_p3():
-    return "The birds chirp to each other.  Max, Charlotte, Eva, Steve, Adam and Amelia have blank looks on their heads. “What is going on!” says Eva. The boys look at each other and realize they are dead.\n"
+    return "The birds chirp to each other.  Gur, Charlotte, Eva, Steve, Adam and Amelia have blank looks on their heads. “What is going on!” says Eva. The boys look at each other and realize they are dead.\n"
 
 gur_p3()
     
