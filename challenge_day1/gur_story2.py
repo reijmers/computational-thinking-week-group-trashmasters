5774d6e1149464170be2d18ef4033b796216e125
MrRyyper <126685166+MrRyyper@users.noreply.github.com>
2023-09-04 16:39:30 +0200

    p2

---
+++ challenge_day1/gur_story2.py
@@ -0,0 +1,2 @@
+def gur_p2():
+    return "Peter the waiter told Amelia and Adam that they should bring some ihce cream back for Max, Charlotte, and Eva. Steve was currently flying all around his building. Some neighvors were scared by Steve’s behaviour since he kept flying around and screaming “you can’t catch me!”"
