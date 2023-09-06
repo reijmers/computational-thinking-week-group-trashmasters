55fcc44edb55f1f035b90f6b3347c890be2031ed
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-06 13:12:00 +0200

    file path

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_excel("C:\\Users\\Gur Levy\\computational-thinking-week-group-trashmasters\\challenge_day3\\namesgroups.xlsx")
+data = pd.read_excel("./challenge_day3/namesgroups.xlsx")
 
 def solution_station_5(name):
     row = data[data["Names"] == name]
