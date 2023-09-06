84c0fcdd4921ad1113df7c3974242e12c4f5707d
mariapenad <mariajosepenad@gmail.com>
2023-09-06 12:23:17 +0200

    function & observations

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -4,7 +4,9 @@ def solution_station_6(input_val):
     output_val = math.sin(input_val)
     return output_val 
 
-# observations of all inputs
+print(f"Input:{input_val}, Output:{output_val}")
+      
+# observations of inputs
 observation1 = (16)
 observation2 = (70)
 observation3 = (96)
