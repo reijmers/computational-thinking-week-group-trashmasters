28fea662c26c9e58b4d9b7b10cc3714bb12df1a2
mariapenad <mariajosepenad@gmail.com>
2023-09-06 13:02:49 +0200

    testing erased, function and comments

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -3,9 +3,9 @@ import math
 def solution_station_6(input): 
     output = math.sin(input)
     return output 
-output_value = solution_station_6(input)
 
-print(f"Input Value:{input}, Output Value:{output}")
+# station 6 may be defined by a sin function 
+# therefore, the output is the sin of the input number 
       
 
 
