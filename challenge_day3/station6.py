e9e02b990f9a4ab7cfe890cb14fab28c6e4c8371
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-06 14:36:56 +0200

    gahdamn rounding

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -2,7 +2,7 @@ import math
 
 def solution_station_6(input): 
     output = math.sin(input)
-    return output 
+    return round(output, 4) 
 
 # station 6 may be defined by a sin function 
 # therefore, the output is the sin of the input number 
