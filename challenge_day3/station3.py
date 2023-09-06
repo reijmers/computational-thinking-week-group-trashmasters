d66b239d970d5a21b3209c42f6eec7ffe2cc1e9b
EmilyPalmm <126685033+EmilyPalmm@users.noreply.github.com>
2023-09-06 12:03:59 +0200

    small change

--- challenge_day3/station3.py
+++ challenge_day3/station3.py
@@ -1,7 +1,7 @@
 import random
 
 def solution_station_3():
-    random_number = random.randint(1, 100000)
+    random_number = random.randint(1, 100)
 
     if random_number % 3 == 0:
         return "true"
