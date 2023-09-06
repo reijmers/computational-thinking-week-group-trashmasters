43f46bcd01aee2958b66bdac83d4cc1b01131eda
sabrina-liu <sabrina.liu@student.uva.nl>
2023-09-06 13:38:50 +0200

    station 6

--- challenge_day3/station5.py
+++ challenge_day3/station5.py
@@ -1,19 +1,29 @@
 import pandas as pd
 
-data = pd.read_excel("./namesgroups.xlsx")
+a=['Mika Maria Isis Rosa Maja Damaris',
+   'Emily Celia Carine Sabrina Maria Gur',
+   'Elisa Sari Dave Dima Jesper Martyna',
+    'Kelt Sebastian Quanpu Ruben Sofia Gabriella', 
+    'Kian Sahir Tom Gonzalo Ameer Teun', 
+    'Angelica Matas Caleb Angeline Raven Paulina', 
+    'Martyna Maja Mate Vincent Eryk Emma', 
+    'Hielke Liss Beatris Caio Sally Sanne', 
+    'Atlas Elli Felix Diana Yash', 
+    'Akanksha Charlie Andrey Max Hugo Al-fatihi', 
+    'Misha Ioanna Ella Cristian Alicja Vanessa', 
+    'Luca Rachna Jelle Karolina Yuyue Alexia']
+
+
 
 def solution_station_5(name):
-    row = data[data["Names"] == name]
-    if not row.empty:
-        return row.iloc[0]["Groups"]
-    else:
-        return None
+    ans=0
+    for i in range(0, 12):
+        if name in a[i]:
+            ans=i
+    return int(ans+1)
     
-name = "Gur"
 
-group_number = solution_station_5(name)
-
-print(group_number)
+print(solution_station_5("Ioanna"))
 
 #function outputs the group number that the person is in
 
