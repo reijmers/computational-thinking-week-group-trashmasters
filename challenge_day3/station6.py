c5272ce9c9e833afeb3a32209891b621ff975438
mariapenad <mariajosepenad@gmail.com>
2023-09-06 12:51:27 +0200

    changes, just have to fix one def

--- challenge_day3/station6.py
+++ challenge_day3/station6.py
@@ -1,10 +1,11 @@
 import math
 
-def solution_station_6(input_value): 
-    output = math.sin(input_value)
+def solution_station_6(input): 
+    output = math.sin(input)
     return output 
+output_value = solution_station_6(input)
 
-print(f"Input Value:{input_value}, Output Value:{output}")
+print(f"Input Value:{input}, Output Value:{output}")
       
 
 
