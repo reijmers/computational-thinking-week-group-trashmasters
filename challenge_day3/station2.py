b01ef77cf9cfe4287097b356ea814ddb64b4c524
Carine Reijmers <carinereijmers@gmail.com>
2023-09-06 13:03:49 +0200

    edit

--- challenge_day3/station2.py
+++ challenge_day3/station2.py
@@ -1,12 +1,19 @@
-import calendar
-from datetime import date
+import datetime
 
 def solution_station_2():
+    date_str = input("Enter a date (YYYY-MM_DD): ")
 
-    d = date.today()
-    print('Date is:', d)
+    try:
+        date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d")
 
-    x = calendar.day_name[d.weekday()]
-    print('Weekday name is:', x)
+        day_of_week = date_obj.weekday()
 
-    #月曜日 日曜日 土曜日 水曜日 火曜日 金曜日 木曜日
+        day_name = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"][day_of_week]
+
+        return date_str, day_name
+    
+    except ValueError:
+        return None
+    
+result = solution_station_2()
+print(f"The day of the week for {date_str} is {day_name}.")
