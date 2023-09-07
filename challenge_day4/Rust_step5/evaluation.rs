b602bb01e8a6418f14f9eaaf2b4556c925dd0c79
MrRyyper <126685166+MrRyyper@users.noreply.github.com>
2023-09-07 16:06:25 +0200

    ff

--- challenge_day4/Rust_step5/evaluation.rs
+++ challenge_day4/Rust_step5/evaluation.rs
@@ -1,36 +1,35 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 use std::path::Path;
 use std::fs::OpenOptions;
-use std::io::Write;
 
 fn main() -> io::Result<()> {
-    let path = Path::new("data5.txt");
+    let path = Path::new("C:\Users\Gur Levy\computational-thinking-wee-group-trashmasters\data5.txt");
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
-    
+
     let mut output = OpenOptions::new()
         .write(true)
         .create(true)
         .open("data6.txt")?;
-    
-    for (indexing, line) in reader.lines().enumerate() {
+
+    for (line_number, line) in reader.lines().enumerate() {
         let line = line?;
-        
-        if index == 0 {
+
+        if line_number == 0 {
             // This is the header, add "Evaluation" to it and write to file
             writeln!(output, "{},Evaluation", line)?;
             continue;
         }
-        
+
         let parts: Vec<&str> = line.split(',').collect();
         if parts.len() < 7 {
             continue; // Skip invalid lines
         }
-        
+
         let mut total_score = 0;
         let mut num_skills = 0;
-        
+
         for &skill in &parts[1..=5] {
             match skill {
                 "low" => {
@@ -52,16 +51,16 @@ fn main() -> io::Result<()> {
                 _ => (), // Skip if it's something else
             }
         }
-        
+
         let evaluation = if num_skills > 0 {
             (total_score as f32) / (num_skills as f32)
         } else {
             0.0 // or whatever you want to set it to if no skills are evaluated
         };
-        
+
         writeln!(output, "{},{},{}", line, parts[6], evaluation)?;
     }
-    
+
     Ok(())
 }
 
